a10124e93cf209f801a2893a5aa424c54af5ab3d
gonicolas <nicolas.gouy@ynov.com>
2024-03-01 16:56:40 +0100

    Artist info front update and search title updated

---
+++ web/assets/js/lightmode2.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleIcon = document.getElementById('theme-toggle');
+    toggleIcon.addEventListener('click', function() {
+        document.body.classList.toggle('light-mode');
+
+        if (document.body.classList.contains('light-mode')) {
+            this.style.color = '#FFA500'; // Icône jaune en mode clair
+            // Appliquer les styles du mode clair
+            applyLightModeStyles();
+        } else {
+            this.style.color = '#fcfcfc'; // Icône blanche en mode sombre
+            // Réappliquer les styles du mode sombre
+            applyDarkModeStyles();
+        }
+    });
+});
+
+function applyLightModeStyles() {
+    document.body.style.backgroundColor = '#f5f5f5';
+    document.querySelectorAll('h1').forEach(el => el.style.color = '#1DB954');
+    document.querySelectorAll('.card-container').forEach(el => el.style.backgroundColor = '#fff');
+    document.querySelectorAll('.artist-card').forEach(el => el.style.backgroundColor = '#fff');
+    document.querySelectorAll('p').forEach(el => el.style.color = '#555');
+    // Continuez avec d'autres éléments...
+}
+
+function applyDarkModeStyles() {
+    document.body.style.backgroundColor = '#333';
+    document.querySelectorAll('h1').forEach(el => el.style.color = '#1DB954');
+    document.querySelectorAll('.card-container').forEach(el => el.style.backgroundColor = '#333');
+    document.querySelectorAll('.artist-card').forEach(el => el.style.backgroundColor = '#1f1f1f');
+    document.querySelectorAll('p').forEach(el => el.style.color = '#ffffff');
+    // Continuez avec d'autres éléments...
+}
